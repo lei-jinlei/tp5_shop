8fdd4e8f78903535a60f74bd997ce430d1d9740f
lei-jinlei <leijinlei_phper@163.com>
2017-05-31 23:36:00 +0800

    尝试添加前台模板文件
    尝试添加前台模板文件

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -1,11 +1,22 @@
 <?php
 namespace app\index\controller;
+use think\Controller;
 
-class Index
+class Index extends Controller
 {
     
     public function index()
     {
-      return '测试是否调用成功';
+    	$this->assign('title','title');
+    	$this->assign('controler','controler');
+    	$city['name'] = 'city.name';
+    	$this->assign('city',$city);
+    	$this->assign('citys','citys');
+    	$this->assign('user','');
+    	$this->assign('cats','');
+    	$this->assign('meishicates','');
+    	$this->assign('datas','');
+
+      	$this->fetch();
     }
 }
